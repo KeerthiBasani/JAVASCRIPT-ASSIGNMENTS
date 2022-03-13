7456433c1022f161a9f856ad08a779dc306ab7ca
basani keerthi <basanikeerthireddy0507@gmail.com>
2022-03-13 22:34:05 +0530

    assignment8 submission

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,29 +1,62 @@
-let count=0;
+let getList = document.getElementById('Main')
+getList.addEventListener('click',addCompleteList)
 
-document.getElementsByTagName("button")[0].addEventListener('click',function(){
-    let data=document.getElementsByTagName("input")[0].value;
-    if(data=="")
+
+let parent1 = document.getElementById('completed')
+let parent2 = document.getElementById('pending')
+
+let btnComplete = document.getElementById('btn1')
+let btnPending = document.getElementById('btn2')
+
+btnComplete.addEventListener('click',ShowTrue)
+btnPending.addEventListener('click',ShowFalse)
+getList.addEventListener('click',showAll)
+
+function addCompleteList()
 {
-    alert("Empty")
+    let getInfo = new XMLHttpRequest();
+
+    getInfo.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+    getInfo.send();
+
+    getInfo.onload = function() { 
+        let list = this.responseText
+        list = JSON.parse(list)
+        for(ele of list){
+            let divElem = document.createElement('div')
+            divElem.innerHTML = `<p><b>${ele.title}</b><p><p>userId:${ele.userId}</p><p>id:${ele.id}</p>
+            `
+            divElem.setAttribute('style',"width: 45%; margin:5px;border: 1px solid white; padding:5px; display:inline-block;")
+
+            if (ele.completed===true){
+                divElem.setAttribute('class','complete')
+                // divElem.setAttribute('style',"color: rgb(79, 161, 79);")
+                divElem.innerHTML += `<p>status:${ele.completed}`
+                parent1.appendChild(divElem)
+            }
+            else if (ele.completed===false){
+                divElem.setAttribute('class','pend')
+                // divElem.setAttribute('style',"color: rgb(219, 62, 62);")
+                divElem.innerHTML += `<p>status:${ele.completed}`
+                parent2.appendChild(divElem)
+            }
+        }
+
+    }
+    
 }
-else{
-    let div_data=document.getElementsByClassName("todos")[0];
-    
-    let p=document.createElement("p");
-    p.setAttribute('key',count);
-    //let key=0;
-    count+=1;
-    p.textContent=document.getElementsByTagName("input")[0].value;
-    div_data.appendChild(p);
-    document.getElementsByTagName("input")[0].value="";
 
-    p.addEventListener('click',function(){
-        div_data.removeChild(p);
-    
-    })
-    
-    
+function ShowTrue(){
+parent2.setAttribute('style','display:None')
+parent1.setAttribute('style','display:inline-block')
 }
 
+function ShowFalse(){
+parent1.setAttribute('style','display:None')
+parent2.setAttribute('style','display:inline-block')
+}
 
-})
+function showAll(){
+parent1.setAttribute('style','display:inline-block')
+parent2.setAttribute('style','display:inline-block')
+}
